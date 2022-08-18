72b4be152df1d5884d47fee02c2ca268449dcd7e
Iryna Semenenko <iryna.semenenko90@gmail.com>
2022-08-18 16:03:29 +0300

    Remove all empty lines

--- ClassMethod/src/UniversityGroup.java
+++ ClassMethod/src/UniversityGroup.java
@@ -5,46 +5,37 @@ public class UniversityGroup {
     private final int startYear;
     private int endYear;
     private String[] studentArray;
-
     public String getGroupName() {
         return groupName;
     }
-
     public int getStartYear() {
         return startYear;
     }
-
     public String[] getStudentArray() {
         return studentArray;
     }
-
     public int getEndYear() {
         return endYear;
     }
-
     public int setEndYear() {
         return startYear + 5;
     }
-
     public UniversityGroup(String groupName, int startYear) {
         this.groupName = groupName;
         this.startYear = startYear;
         setEndYear();
     }
-
     public UniversityGroup(String groupName, int startYear, String[] studentArray) {
         this.groupName = groupName;
         this.startYear = startYear;
         this.studentArray = studentArray;
         setEndYear();
     }
-
     public String[] addStudents() {
         String[] students;
         students = new String[]{"Nikita", "Anatoliy", "Olga"};
         return students;
     }
-
     public void getGroupDescription() {
         if (getStudentArray() == null) {
             System.out.println(groupName);
