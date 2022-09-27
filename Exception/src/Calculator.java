a83d054abf4a9fa0a61246880757ec73fc817c32
Iryna Semenenko <iryna.semenenko90@gmail.com>
2022-09-27 23:40:22 +0300

    Updated commit with comments

--- Exception/src/Calculator.java
+++ Exception/src/Calculator.java
@@ -1,51 +1,16 @@
 public class Calculator {
     public static void main(String[] args) {
         MethodsCalculator methodsCalculator = new MethodsCalculator();
-        double firstValue, secondValue, result;
-        boolean invalidOperation = true;
-        String operation = "";
-        System.out.println("It is a simple calculator.");
-        System.out.println("It performs simple arithmetic calculations using operations (+, -, /, *, %) and supports non-integer numbers (4,5)\n");
-        System.out.println("Pleas, enter the first value: ");
-        firstValue = methodsCalculator.setValue();
+        System.out.println("It is a simple calculator. " +
+                "\nIt performs simple arithmetic calculations using operations (+, -, /, *, %) " +
+                "and supports non-integer numbers (4,5)\n" +
+                "\nPlease, enter the first value: ");
+        methodsCalculator.getFirstValue();
         System.out.println("Pleas, enter the operation value: ");
-        while (invalidOperation) {
-            try {
-                operation = methodsCalculator.setOperation();
-                invalidOperation = false;
-            } catch (NotSupportedOperation notSupportedOperation) {
-                System.err.println("Error: Invalid operation. Should be +, -, /, *, % operations. ");
-                System.out.println("Let's try again! Please, enter the operation:");
-            }
-        }
+        methodsCalculator.checkOperation();
         System.out.println("Enter the second value: ");
-        secondValue = methodsCalculator.setValue();
-        switch (operation) {
-            case "+" -> {
-                result = firstValue + secondValue;
-                System.out.println(firstValue + " + " + secondValue + " = " + result);
-            }
-            case "-" -> {
-                result = firstValue - secondValue;
-                System.out.println(firstValue + " - " + secondValue + " = " + result);
-            }
-            case "*" -> {
-                result = firstValue * secondValue;
-                System.out.println(firstValue + " * " + secondValue + " = " + result);
-            }
-            case "/" -> {
-                if (secondValue != 0) {
-                    result = firstValue / secondValue;
-                    System.out.println(firstValue + " / " + secondValue + " = " + result);
-                } else {
-                    System.err.println(firstValue + " / " + secondValue + " = " + "Error: Divide by zero is impossible.");
-                }
-            }
-            case "%" -> {
-                result = firstValue % secondValue;
-                System.out.println(firstValue + " % " + secondValue + " = " + result);
-            }
-        }
+        methodsCalculator.getSecondValue();
+        methodsCalculator.getResult();
     }
 }
 
